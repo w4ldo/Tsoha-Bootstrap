7cbe3c402d98f830edb2d504a81644c343d4e59b
Isosaari Matias L J <imatias@da5-cs-bk107-30.pc.helsinki.fi>
2017-05-29 17:10:41 +0300

    createTables

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,34 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Owner (
+  id SERIAL PRIMARY KEY, -- <-- huom! serial
+  username varchar(50) NOT NULL,
+  password varchar(50) NOT NULL
+);
+
+CREATE TABLE Task (
+  id SERIAL PRIMARY KEY,
+  owner_id INTEGER REFERENCES Owner(id),
+  taskname varchar(50) NOT NULL,
+--  played boolean DEFAULT FALSE,
+  description varchar(400),
+);
+
+CREATE TABLE Tag (
+  id SERIAL PRIMARY KEY,
+  tagname varchar(50) NOT NULL,
+);
+
+CREATE TABLE TaskTag (
+  tag_id INTEGER REFERENCES Tag(id) NOT NULL,
+  task_id INTEGER REFERENCES Task(id) NOT NULL
+);
+
+CREATE TABLE Priority (
+  id SERIAL PRIMARY KEY,
+  priorityname varchar(50) NOT NULL,
+);
+
+CREATE TABLE TaskPriority (
+  priority_id INTEGER REFERENCES Priority(id), NOT NULL
+  task_id INTEGER REFERENCES Task(id) NOT NULL
+);
