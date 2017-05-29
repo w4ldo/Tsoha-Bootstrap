632476db175a6d1a7d898af61d402a9611d3607d
Isosaari Matias L J <imatias@da5-cs-bk107-30.pc.helsinki.fi>
2017-05-29 13:40:43 +0300

    views

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -1,14 +1,31 @@
 <?php
 
-  class HelloWorldController extends BaseController{
+class HelloWorldController extends BaseController {
 
-    public static function index(){
-      // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+    public static function index() {
+        // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
+        View::make('home.html');
     }
 
-    public static function sandbox(){
-      // Testaa koodiasi täällä
-      echo 'Hello World!';
+    public static function sandbox() {
+        // Testaa koodiasi täällä
+        View::make('helloworld.html');
     }
-  }
+    
+    public static function task_edit() {
+        View::make('suunnitelmat/task_edit.html');
+    }
+
+    public static function task_list() {
+        View::make('suunnitelmat/task_list.html');
+    }
+
+    public static function show() {
+        View::make('suunnitelmat/task_show.html');
+    }
+
+    public static function login() {
+        View::make('suunnitelmat/login.html');
+    }
+
+}
--- config/routes.php
+++ config/routes.php
@@ -1,9 +1,24 @@
 <?php
 
-  $routes->get('/', function() {
+$routes->get('/', function() {
     HelloWorldController::index();
-  });
+});
 
-  $routes->get('/hiekkalaatikko', function() {
+$routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
-  });
+});
+
+$routes->get('/task', function() {
+    HelloWorldController::task_list();
+});
+$routes->get('/task/1', function() {
+    HelloWorldController::task_show();
+});
+
+$routes->get('/taskedit', function() {
+    HelloWorldController::task_edit();
+});
+
+$routes->get('/login', function() {
+    HelloWorldController::login();
+});
