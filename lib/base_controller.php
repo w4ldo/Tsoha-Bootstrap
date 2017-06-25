95387747173664ac085cac83d2d00ade7c798f15
w4ldo <w4ldo@users.noreply.github.com>
2017-06-25 17:49:24 +0300

    Update base_controller.php

--- lib/base_controller.php
+++ lib/base_controller.php
@@ -3,16 +3,13 @@
 class BaseController {
 
     public static function get_user_logged_in() {
-        // Katsotaan onko user-avain sessiossa
         if (isset($_SESSION['owner'])) {
             $owner_id = $_SESSION['owner'];
-            // Pyydetään User-mallilta käyttäjä session mukaisella id:llä
             $owner = Owner::find($owner_id);
 
             return $owner;
         }
 
-        // Käyttäjä ei ole kirjautunut sisään
         return null;
     }
 
