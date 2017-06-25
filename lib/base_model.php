c59974c9b33d524732b8d9762191849c81fad90e
w4ldo <w4ldo@users.noreply.github.com>
2017-06-25 17:49:40 +0300

    Update base_model.php

--- lib/base_model.php
+++ lib/base_model.php
@@ -3,11 +3,8 @@
 class BaseModel {
 
     public function __construct($attributes = null) {
-        // Käydään assosiaatiolistan avaimet läpi
         foreach ($attributes as $attribute => $value) {
-            // Jos avaimen niminen attribuutti on olemassa...
             if (property_exists($this, $attribute)) {
-                // ... lisätään avaimen nimiseen attribuuttin siihen liittyvä arvo
                 $this->{$attribute} = $value;
             }
         }
