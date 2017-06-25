0030c65d6afaca053b8216972ea267fbcc788a5b
w4ldo <w4ldo@users.noreply.github.com>
2017-06-25 17:50:39 +0300

    Update view.php

--- lib/view.php
+++ lib/view.php
@@ -3,22 +3,17 @@
   class View{
 
     public static function make($view, $content = array()){
-      // Alustetaan Twig
       $twig = self::get_twig();
 
       try{
-        // Asetetaan uudelleenohjauksen yhteydessä lisätty viesti
         self::set_flash_message($content);
 
-        // Asetetaan näkymään base_path-muuttuja index.php:ssa määritellyllä BASE_PATH vakiolla
         $content['base_path'] = BASE_PATH;
 
-        // Asetetaan näkymään kirjautunut käyttäjä, jos get_user_logged_in-metodi on toteutettu
         if(method_exists('BaseController', 'get_user_logged_in')){
           $content['user_logged_in'] = BaseController::get_user_logged_in();
         }
 
-        // Tulostetaan Twig:n renderöimä näkymä
         echo $twig->render($view, $content);
       } catch (Exception $e){
         die('Virhe näkymän näyttämisessä: ' . $e->getMessage());
