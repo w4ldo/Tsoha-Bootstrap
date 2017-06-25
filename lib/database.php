f9d5bf22ccb2fc09c1bc6dca273e903dacaa0ad3
w4ldo <w4ldo@users.noreply.github.com>
2017-06-25 17:50:04 +0300

    Update database.php

--- lib/database.php
+++ lib/database.php
@@ -3,21 +3,16 @@
   class DB{
 
       public static function connection(){
-        // Haetaan tietokantakonfiguraatio
         $connection_config = DatabaseConfig::connection_config();
         $config = $connection_config['config'];
 
         try {
-            // Alustetaan PDO
             if(isset($config['username'])){
               $connection = new PDO($config['resource'], $config['username'], $config['password']);
             }else{
               $connection = new PDO($config['resource']);
             }
-             // Asetetaan tietokannan kenttien koodaukseksi utf8
             $connection->exec('SET NAMES UTF8');
-
-            // Näytetään virheilmoitukset
             $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $connection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
             
